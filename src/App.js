88334e93af3e1d3375db9e9ce26a946d78f1d09a
pranav2805 <pranavpradeep095@gmail.com>
2023-08-11 14:28:29 +0530

    Making Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -7,36 +7,47 @@ function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
+      location: "Mumbai",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Delhi",
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
+      location: "Bangalore",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
+      location: "Mumbai",
     },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* {expenses.forEach((item) => {
+      {expenses.forEach((item) => {
         <ExpenseItem
           title={item.title}
           amount={item.amount}
           date={item.date}
-        ></ExpenseItem>;
-      })} */}
-      <ExpenseItem
+          location={item.location}
+        />;
+      })}
+      {/* <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+        location={expenses[0].location}
+      ></ExpenseItem> */}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,20 @@
-import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
-  const locOfExpense = "Mumbai";
+  // const locOfExpense = "Mumbai";
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <div className="expense-item__description">{locOfExpense}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        amount={props.amount}
+        location={props.location}
+        title={props.title}
+      />
     </div>
   );
 }
