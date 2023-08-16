36fd006f777b6d04627e3b3ff65bbd5a5ebfddfc
pranav2805 <pranavpradeep095@gmail.com>
2023-08-16 20:55:55 +0530

    Creating expense form component & onchange event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -43,8 +44,8 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}></Expenses>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 };
