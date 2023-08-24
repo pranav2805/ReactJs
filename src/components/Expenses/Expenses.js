6c1c091961b5211cb2567260a3276018e40d0030
pranav2805 <pranavpradeep095@gmail.com>
2023-08-24 22:43:12 +0530

    Introducing charts in the app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -36,6 +37,7 @@ const Expenses = (props) => {
             />
           );
         })} */}
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
